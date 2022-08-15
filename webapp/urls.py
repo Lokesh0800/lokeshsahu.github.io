b3c4cf788a7a5c337d5b715acb69d36a22005547
Lokesh0800 <61263888+Lokesh0800@users.noreply.github.com>
2022-08-15 13:44:19 +0530

    Update urls.py
    1st commit

--- webapp/urls.py
+++ webapp/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('homeapp.urls'))
+    path('lokeshsahu.github.io',include('homeapp.urls'))
 ]
